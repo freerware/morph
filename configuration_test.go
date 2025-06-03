6c0853b8ad11e2836ed58faf9fdc3208bc249d94
fr33r <github@fr33r.com>
2025-06-02 22:00:06 -0700

    Support specifying references in configuration files.

--- configuration_test.go
+++ configuration_test.go
@@ -31,6 +31,44 @@ func (s *ConfigurationTestSuite) TestAsMetadata() {
 						FieldStrategy: morph.FieldStrategyStructField,
 						PrimaryKey:    true,
 					},
+					{
+						Name:          "business_name",
+						Field:         "BusinessName",
+						FieldType:     "string",
+						FieldStrategy: morph.FieldStrategyStructField,
+						PrimaryKey:    false,
+					},
+				},
+				References: []morph.ReferenceConfiguration{
+					{
+						TableName: "business",
+						ForeignKey: []morph.ForeignKeyConfiguration{
+							{
+								ColumnName: "business_name",
+							},
+						},
+					},
+				},
+			},
+			{
+				TypeName: "example.Business",
+				Name:     "business",
+				Alias:    "B",
+				Columns: []morph.ColumnConfiguration{
+					{
+						Name:          "name",
+						Field:         "Name",
+						FieldType:     "string",
+						FieldStrategy: morph.FieldStrategyStructField,
+						PrimaryKey:    true,
+					},
+					{
+						Name:          "user_count",
+						Field:         "UserCount",
+						FieldType:     "int",
+						FieldStrategy: morph.FieldStrategyStructField,
+						PrimaryKey:    false,
+					},
 				},
 			},
 		},
@@ -40,15 +78,55 @@ func (s *ConfigurationTestSuite) TestAsMetadata() {
 	tables := config.AsMetadata()
 
 	// assert.
-	s.Require().Len(config.Tables, len(tables))
-	s.Equal(config.Tables[0].TypeName, tables[0].TypeName())
-	s.Equal(config.Tables[0].Name, tables[0].Name())
-	s.Equal(config.Tables[0].Alias, tables[0].Alias())
+	tablesByName := make(map[string]morph.Table)
+	for _, table := range tables {
+		tablesByName[table.Name()] = table
+	}
 
-	s.Require().Len(config.Tables[0].Columns, len(tables[0].Columns()))
-	s.Equal(config.Tables[0].Columns[0].Name, tables[0].Columns()[0].Name())
-	s.Equal(config.Tables[0].Columns[0].Field, tables[0].Columns()[0].Field())
-	s.Equal(config.Tables[0].Columns[0].FieldType, tables[0].Columns()[0].FieldType())
-	s.True(tables[0].Columns()[0].UsingStructFieldStrategy())
-	s.Equal(config.Tables[0].Columns[0].PrimaryKey, tables[0].Columns()[0].PrimaryKey())
+	s.Require().Len(config.Tables, len(tables))
+
+	userTableConfig := config.Tables[0]
+	businessTableConfig := config.Tables[1]
+	userTable := tablesByName[config.Tables[0].Name]
+	businessTable := tablesByName[config.Tables[1].Name]
+
+	s.Equal(userTableConfig.TypeName, userTable.TypeName())
+	s.Equal(userTableConfig.Name, userTable.Name())
+	s.Equal(userTableConfig.Alias, userTable.Alias())
+
+	s.Equal(businessTableConfig.TypeName, businessTable.TypeName())
+	s.Equal(businessTableConfig.Name, businessTable.Name())
+	s.Equal(businessTableConfig.Alias, businessTable.Alias())
+
+	s.Require().Len(userTableConfig.Columns, len(userTable.Columns()))
+	for _, columnConfig := range userTableConfig.Columns {
+		s.Require().True(userTable.HasColumn(columnConfig.Name))
+		column := userTable.FindColumns(func(c morph.Column) bool { return c.Name() == columnConfig.Name })[0]
+		s.Equal(columnConfig.Name, column.Name())
+		s.Equal(columnConfig.Field, column.Field())
+		s.Equal(columnConfig.FieldType, column.FieldType())
+		s.True(column.UsingStructFieldStrategy())
+
+		if columnConfig.PrimaryKey {
+			s.True(column.PrimaryKey())
+		}
+	}
+
+	for _, columnConfig := range businessTableConfig.Columns {
+		s.Require().True(businessTable.HasColumn(columnConfig.Name))
+		column := businessTable.FindColumns(func(c morph.Column) bool { return c.Name() == columnConfig.Name })[0]
+		s.Equal(columnConfig.Name, column.Name())
+		s.Equal(columnConfig.Field, column.Field())
+		s.Equal(columnConfig.FieldType, column.FieldType())
+		s.True(column.UsingStructFieldStrategy())
+
+		if columnConfig.PrimaryKey {
+			s.True(column.PrimaryKey())
+		}
+	}
+
+	s.Require().Len(userTableConfig.References, len(userTable.ReferencesTo()))
+	s.True(businessTable.IsReferencedBy(userTable))
+	s.Require().Len(userTableConfig.References, len(businessTable.ReferencedBy()))
+	s.True(userTable.HasReferenceTo(businessTable))
 }
